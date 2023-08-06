f6dde29f95b4681de9d7389b8d92a544b63d15f1
Pranav-Patani <pranav.patani@gmail.com>
2023-08-06 12:50:08 +0530

    fix: fixed category issue with product- 'Pomegranate Juice'

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -111,7 +111,7 @@ export const products = [
     volume: "1 L",
     imgUrl:
       "https://images.unsplash.com/photo-1568909344668-6f14a07b56a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
-    categoryName: "Juice",
+    categoryName: "Juices",
     rating: "3",
   },
   {
