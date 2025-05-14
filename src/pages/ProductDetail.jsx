f963f23cc92fdfc7ed540fe78621ec463df27e7b
Pranav-Patani <pranav.patani@gmail.com>
2025-05-15 02:34:14 +0530

    feat: add responsiveness to  product detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -10,7 +10,6 @@ import { addToCartFunc } from "../services/cartServices";
 import { addToWishlistFunc } from "../services/wishlistServices";
 import { useNavigate } from "react-router-dom";
 
-
 export const ProductDetail = () => {
   const navigate = useNavigate();
   const { token } = useAuth();
@@ -20,7 +19,7 @@ export const ProductDetail = () => {
     dispatch,
   } = useData();
   const productFind = beverages.find(({ _id }) => _id === productId);
- 
+
   const { _id, name, brand, price, categoryName, rating, imgUrl } = productFind;
 
   const isInCart = isAlreadyPresentInCart(_id, cart);
@@ -44,77 +43,77 @@ export const ProductDetail = () => {
 
   return (
     <>
-    
       <div className="product-detail">
-        <div className="product-detail__img-container">
-          <img
-            src={imgUrl}
-            alt="product"
-            className="product-detail__img-container__img"
-          />
-        </div>
-        <div className="product-detail__text-container">
-          <h3 className="product-detail__text-container__name heading-tertiary">
-            {name}
-          </h3>
-          <h2 className="product-detail__text-container__price heading-secondary">
-            &#8377;{price}
-          </h2>
-
-          <div className="product-detail__text-container__table">
-            <div className="product-detail__text-container__table--left">
-              <p className="product-detail__text-container__table--left__text">
-                Brand
-              </p>
-              <p className="product-detail__text-container__table--left__text">
-                Rating
-              </p>
-              <p className="product-detail__text-container__table--left__text">
-                Category
-              </p>
-            </div>
-            <div className="product-detail__text-container__table--right">
-              <p className="product-detail__text-container__table--right__text">
-                {brand}
-              </p>
-              <p className="product-detail__text-container__table--right__text">
-                {rating}&#9733;
-              </p>
-              <p className="product-detail__text-container__table--right__text">
-                {categoryName}
-              </p>
-            </div>
+        <div className="product-detail__wrapper">
+          <div className="product-detail__img-container">
+            <img
+              src={imgUrl}
+              alt="product"
+              className="product-detail__img-container__img"
+            />
           </div>
-
-          <div className="product-detail__text-container__details">
-            <h3 className="product-detail__text-container__details__heading heading-tertiary">
-              Details
+          <div className="product-detail__text-container">
+            <h3 className="product-detail__text-container__name heading-tertiary">
+              {name}
             </h3>
-            <p className="product-detail__text-container__details__text">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
-              asperiores voluptatum, ea quos laudantium minima ducimus ut
-              impedit quisquam accusantium error commodi quibusdam corporis
-              temporibus id corrupti eos doloribus voluptate.
-            </p>
-          </div>
-          <div className="product-detail__text-container__btn-container">
-            <button
-              onClick={addtoCartHandler}
-              className="product-detail__text-container__btn-container__btn btn-tertiary"
-            >
-              {isInCart ? "Go to Cart" : "Add to Cart"}
-            </button>
+            <h2 className="product-detail__text-container__price heading-secondary">
+              &#8377;{price}
+            </h2>
 
-            <button
-              onClick={addToWishlistHandler}
-              className="product-detail__text-container__btn-container__btn btn-tertiary"
-            >
-              {isInWishlist ? "Go to Wishlist" : "Add to Wishlist"}
-            </button>
+            <div className="product-detail__text-container__table">
+              <div className="product-detail__text-container__table--left">
+                <p className="product-detail__text-container__table--left__text">
+                  Brand
+                </p>
+                <p className="product-detail__text-container__table--left__text">
+                  Rating
+                </p>
+                <p className="product-detail__text-container__table--left__text">
+                  Category
+                </p>
+              </div>
+              <div className="product-detail__text-container__table--right">
+                <p className="product-detail__text-container__table--right__text">
+                  {brand}
+                </p>
+                <p className="product-detail__text-container__table--right__text">
+                  {rating}&#9733;
+                </p>
+                <p className="product-detail__text-container__table--right__text">
+                  {categoryName}
+                </p>
+              </div>
+            </div>
+
+            <div className="product-detail__text-container__details">
+              <h3 className="product-detail__text-container__details__heading heading-tertiary">
+                Details
+              </h3>
+              <p className="product-detail__text-container__details__text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Possimus asperiores voluptatum, ea quos laudantium minima
+                ducimus ut impedit quisquam accusantium error commodi quibusdam
+                corporis temporibus id corrupti eos doloribus voluptate.
+              </p>
+            </div>
+            <div className="product-detail__text-container__btn-container">
+              <button
+                onClick={addtoCartHandler}
+                className="product-detail__text-container__btn-container__btn btn-tertiary"
+              >
+                {isInCart ? "Go to Cart" : "Add to Cart"}
+              </button>
+
+              <button
+                onClick={addToWishlistHandler}
+                className="product-detail__text-container__btn-container__btn btn-tertiary"
+              >
+                {isInWishlist ? "Go to Wishlist" : "Add to Wishlist"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
- 
     </>
   );
 };
